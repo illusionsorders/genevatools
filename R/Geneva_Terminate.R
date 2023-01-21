7b5e7dcdb444f09afdd85b656c1c4e3ec14a4c77
illusions+orders <123269573+illusionsorders@users.noreply.github.com>
2023-01-21 13:15:47 -0800

    fixed geneva_Terminate

--- R/Geneva_Terminate.R
+++ R/Geneva_Terminate.R
@@ -1,7 +1,6 @@
 geneva_Terminate<-function(hours){
-  hours<-dlgInput("How many hours to execute?", Sys.info()[""])$res
-  hours<-as.numeric(time)
-  hours<-time*60^2
+  hours<-as.numeric(hours)
+  hours<-hours*60^2
 
   script<-paste("shutdown -s -t", hours)
   system(script)
